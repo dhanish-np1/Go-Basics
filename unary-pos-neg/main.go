725464485dd1c2a0d6065f058103713493b1357b
Dhanish NP <dhanishd947@gmail.com>
2025-08-19 08:06:42 +0530

    completed: unary positive and unary negative

---
+++ unary-pos-neg/main.go
@@ -0,0 +1,14 @@
+package main 
+
+import "fmt"
+
+func main() {
+	w := 1
+	x := 1
+	y := 1.00
+	z := 1.00
+	fmt.Println(+w) // 1
+	fmt.Println(-x) // -1
+	fmt.Println(+y) // 1
+	fmt.Println(-z) // -1
+}
